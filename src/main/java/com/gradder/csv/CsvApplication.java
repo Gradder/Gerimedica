8f409935d45bd0035d51784cb254c83114088e2e
rz <rzhitkov@gmail.com>
2024-11-07 16:27:49 +0100

    Cleanup

--- src/main/java/com/gradder/csv/CsvApplication.java
+++ src/main/java/com/gradder/csv/CsvApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GerimedicaApplication {
+public class CsvApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GerimedicaApplication.class, args);
+		SpringApplication.run(CsvApplication.class, args);
 	}
 
 }
--- src/test/java/com/gradder/csv/CsvApplicationTests.java
+++ src/test/java/com/gradder/csv/CsvApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GerimedicaApplicationTests {
+class CsvApplicationTests {
 
 	@Test
 	void contextLoads() {
